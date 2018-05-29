346993139b0b3c7edbc0625991e485fd9d8eb8e0
Kibir <kibir.belay@gmail.com>
2018-05-29 20:54:58 +0300

    updated way cake details listed and updating cake ...

--- src/components/movie-detail.js
+++ src/components/movie-detail.js
@@ -4,7 +4,7 @@ import {connect} from 'react-redux'
 class MovieDetail extends Component {
  	constructor(props) {
     		super(props);
-		
+	
 		this.handleChange= this.handleChange.bind(this);
 
     		this.state = {
@@ -14,43 +14,55 @@ class MovieDetail extends Component {
 	}
 
 	handleChange(){
-let url = "http://ec2-34-243-153-154.eu-west-1.compute.amazonaws.com:5000/api/cakes" + this.props.movie.id;
 
-var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("PUT",url,false);
-    Httpreq.send({id:this.props.movie.id, name:this.props.movie.name, comment:this.state.comment, imageUrl:this.props.movie.imageUrl, yumFactor:this.state.yumfactor});
-    console.log(JSON.parse(Httpreq.responseText));
-	 }
+		var url = "http://ec2-34-243-153-154.eu-west-1.compute.amazonaws.com:5000/api/cakes/";
+
+		var data = {};
+		data.id = this.props.movie.id;
+		data.name  = this.props.movie.name;
+		data.comment  = this.props.movie.comment;
+		data.imageUrl  = this.props.movie.imageUrl;
+		data.yumFactor  = this.state.yumfactor;
+
+		var json = JSON.stringify(data);
+
+		var xhr = new XMLHttpRequest();
+		xhr.open("PUT", url+ this.props.movie.id, true);
+		xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+		xhr.send(json);
+
+	}
 
 	render() {
 		console.log(this.state.comment);
 		console.log(this.state.yumfactor);
 		if (!this.props.movie) {
 			return (
-				<div>Click one of the movies to see details.</div>
+				<div>Click one of the cakes to see details.</div>
 			);
 		}
 		return (
-<div>		<div>
+		<div>
+			<div>
  				<h4>Name: {this.props.movie.name}</h4>
 				<div>Comment: {this.props.movie.comment}</div>
-<div>yumFactor: {this.props.movie.yumFactor}</div>
+				<div>yumFactor: {this.props.movie.yumFactor}</div>
 			</div>
 
-<form onSubmit={this.handleChange}>
-<div>
-<label>Comment:</label>
-<input type="text" value={this.state.comment}  onChange={(e)=>this.setState({comment:e.target.value})} /> 
+		<form onSubmit={this.handleChange}>
+		<div>
+		<label>Comment:</label>
+		<input type="text" value={this.state.comment}  onChange={(e)=>this.setState({comment:e.target.value})} /> 
 </div>
 
-<div>
-<label>Yum Factor</label>
-<input type="text" value={this.state.yumfactor} onChange={(e)=>this.setState({yumfactor:e.target.value})} /> 
-</div>
-<div><button>Add</button></div>
-</form>
+		<div>
+		<label>Yum Factor</label>
+		<input type="text" value={this.state.yumfactor} onChange={(e)=>this.setState({yumfactor:e.target.value})} /> 
+		</div>
+		<div><button>Modify</button></div>
+		</form>
 
-</div>	
+		</div>	
 		);
 	}
 }
